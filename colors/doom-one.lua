d78efde1a9873058788d8298cdfde3852f185e79
Junior Sundar <juniorsundar@gmail.com>
2026-02-07 23:01:22 +0400

    Added some more integrations

--- colors/doom-one.lua
+++ colors/doom-one.lua
@@ -1,9 +1,9 @@
-vim.cmd('hi clear')
-if vim.fn.exists('syntax_on') then
-	vim.cmd('syntax reset')
+vim.cmd("hi clear")
+if vim.fn.exists("syntax_on") then
+  vim.cmd("syntax reset")
 end
 
-vim.g['colors_name'] = 'doom-one'
+vim.g["colors_name"] = "doom-one"
 
-package.loaded['doom-one'] = nil
-require('doom-one').set_colorscheme()
+package.loaded["doom-one"] = nil
+require("doom-one").set_colorscheme()
